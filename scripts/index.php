ae692df7400969443bf3f2999cdb2b5ac12b1c90
Jerry Pirkle <legendary.jerry@gmail.com>
2015-06-06 03:11:11 -0500

    Testing gitignore

---
+++ scripts/index.php
@@ -0,0 +1,14 @@
+Files
+* aa		Memory usage details
+* ab		Counts unique visitors from Apache access_log
+* ac		Lists CPUs, Memory
+* ad    DNS Information
+* ca		Basic apache2 maths
+* ch		Basic HTTP Maths
+* dk		Shows large files
+* du      	Disk usage
+* ip.php	Shows your public IP address
+* ld      	Linux distribution detection
+* rk		Installs and runs rootkit hunter
+* si		Basic system 
+* tr		List directory tree with nice formatting
